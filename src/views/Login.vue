db6a35f92914c99e2e71d5200a6a67ad6842389a
TimothyLawSongEn <e0725564@u.nus.edu>
2023-04-01 17:41:14 +0800

    Cleanup

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,63 @@
 <template>
-    <h1 style="align-items: center;">Welcome to TrackTrip!</h1>
-    <h2 style="align-items: center;">Login Here</h2>
-    <div id = "firebaseui-auth-container" @submit.prevent="setUser"></div>
+
+<section class="vh-100" >
+        <div class="container py-5 h-100">
+          <div class="d-flex justify-content-center align-items-center h-100">
+
+            <div class="col-md d-flex flex-column align-items-center ">
+              <div class="text-center py-5">
+                <h1>New Here?</h1>
+                <br>
+                <p>
+                Sign up and gain complete control over your spending now!</p>
+              </div>
+              <!-- <button class="btn btn-lg btn-block text-light" type="submit" style="background-color: #2196F3;">Sign Up</button> -->
+            </div>
+
+            <div class="col-md">
+              <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                <div class="card-body p-5 text-center">
+
+                  <h3 class="mb-5">Log In</h3>
+
+                  <div id = "firebaseui-auth-container" @submit.prevent="setUser"></div>
+
+                  <!-- <div class="form-outline mb-4">
+                    <input type="email" id="typeEmail" class="form-control form-control-lg" />
+                    <label class="form-label" for="typeEmail">Email</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="typePassword" class="form-control form-control-lg" />
+                    <label class="form-label" for="typePassword">Password</label>
+                  </div> -->
+
+                  <!-- Checkbox -->
+                  <!-- <div class="form-check d-flex justify-content-start mb-4">
+                    <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
+                    <label class="form-check-label px-2" for="form1Example3"> Remember Me</label>
+                  </div>
+
+                  <button class="btn text-light btn-lg btn-block" type="submit" style="background-color: #2196F3;">Login</button>
+
+                  <hr class="my-4">
+
+                  <button class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #dd4b39;"
+                    type="submit"><i class="bi bi-google me-2"></i> Continue with google</button> -->
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+    <!-- <h1 style="align-items: center;">Welcome to TrackTrip!</h1>
+    <h2 style="align-items: center;">Login Here</h2> -->
+    <!-- <div id = "firebaseui-auth-container" @submit.prevent="setUser"></div> -->
  </template>
 
  <script>
@@ -100,6 +156,7 @@
             console.error("Error adding user: ", error)
         }
     }
+
  </script>
 
  <style scoped>
